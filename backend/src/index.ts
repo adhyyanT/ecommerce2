e0076d937a19996426c19f18bb9c2723181af33f
adhyyanT <24794477+adhyyanT@users.noreply.github.com>
2023-09-07 21:39:59 +0530

    changed appdatasource

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
 import app from './app';
-
+import { AppDataSource } from './config/connectDB';
 app.listen(process.env.PORT, () => {
   console.log(`server started on port = ${process.env.PORT}`);
 });
-
-export default app;
