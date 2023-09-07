1b13656bd71a642b0cd53e7eed97d77514421124
adhyyanT <24794477+adhyyanT@users.noreply.github.com>
2023-09-07 20:35:19 +0530

    vercel starter config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import cors from 'cors';
-import passport from './config/passport';
-import userRoutes from './Routes/userRoutes';
-import profileRoutes from './Routes/profileRoutes';
-import { errorHandler } from './middleware/errorHandler';
-import createHttpError from 'http-errors';
-import cartRoutes from './Routes/cartRoutes';
-import orderRoute from './Routes/orderRoutes';
-import productRoute from './Routes/productRoutes';
-dotenv.config();
+import app from './app';
 
-const app = express();
-app.use(passport.initialize());
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.client!,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-    allowedHeaders: '*',
-    exposedHeaders: '*',
-  })
-);
-app.use(morgan('combined'));
-
-app.use('/user', userRoutes);
-app.use(passport.authenticate('jwt', { session: false }));
-app.use('/profile', profileRoutes);
-app.use('/cart', cartRoutes);
-app.use('/order', orderRoute);
-app.use('/products', productRoute);
-
-app.use((req, res, next) => {
-  next(createHttpError(404, 'Page not found'));
-});
-app.use(errorHandler);
 app.listen(process.env.PORT, () => {
   console.log(`server started on port = ${process.env.PORT}`);
 });
+
+export default app;
