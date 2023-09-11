3e669a106909ffcda21776e77e1aaf33bb18b63f
adhyyanT <24794477+adhyyanT@users.noreply.github.com>
2023-09-11 14:26:13 +0530

    added http server

--- email_worker/index.ts
+++ email_worker/index.ts
@@ -2,6 +2,8 @@ import { Job, Queue, Worker } from 'bullmq';
 import dotenv from 'dotenv';
 import IORedis from 'ioredis';
 import nodeMailer from 'nodemailer';
+import http from 'http';
+
 dotenv.config();
 console.log('start');
 const transporter = nodeMailer.createTransport({
@@ -16,29 +18,33 @@ const con = new IORedis(process.env.redis_url!, {
   maxRetriesPerRequest: null,
 });
 
-console.log('[X] Listening ');
-const worker = new Worker(
-  'email_queue',
-  async (job: Job) => {
-    const options = {
-      from: process.env.email_id,
-      to: job.data.emailId,
-      subject: 'Order placed successfully!',
-      text: `summary: ${job.data.emailProducts}`,
-      html: `
-      <h2>Hi ${job.data.name}. This is a confirmation email regarding your purchase of $${job.data.total} on Ecommerce </h2>
-      <p>Thanks & Regards </p>
-      <p>Ecommerce inc.</p>
-        `,
-    };
-    transporter.sendMail(options, (error, info) => {
-      if (error) {
-        console.log(error);
-      }
-      console.log(info.response);
-    });
-  },
-  {
-    connection: con,
-  }
-);
+const server = http.createServer();
+server.listen(3000, () => {
+  console.log('server started on port 3000');
+  console.log('[X] Listening ');
+  const worker = new Worker(
+    'email_queue',
+    async (job: Job) => {
+      const options = {
+        from: process.env.email_id,
+        to: job.data.emailId,
+        subject: 'Order placed successfully!',
+        text: `summary: ${job.data.emailProducts}`,
+        html: `
+            <h2>Hi ${job.data.name}. This is a confirmation email regarding your purchase of $${job.data.total} on Ecommerce </h2>
+            <p>Thanks & Regards </p>
+            <p>Ecommerce inc.</p>
+              `,
+      };
+      transporter.sendMail(options, (error, info) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(info.response);
+      });
+    },
+    {
+      connection: con,
+    }
+  );
+});
