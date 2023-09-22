72f0e5c9e745fcb3f6a342bd0c28473d05c2dbb5
adhyyanT <24794477+adhyyanT@users.noreply.github.com>
2023-09-22 20:23:40 +0530

    updated modules

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import app from './app';
-// import { AppDataSource } from './config/connectDB';
+import dotenv from 'dotenv';
+dotenv.config();
 app.listen(process.env.PORT || 3000, () => {
   console.log(`server started on port = ${process.env.PORT}`);
 });
