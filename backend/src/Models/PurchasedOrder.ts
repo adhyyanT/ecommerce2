e061c7e923c4f21598adb493ae6f9d73741e709c
adhyyanT <24794477+adhyyanT@users.noreply.github.com>
2023-09-24 19:49:52 +0530

    cleanup

--- backend/src/Models/PurchasedOrder.ts
+++ backend/src/Models/PurchasedOrder.ts
@@ -1,39 +1,3 @@
-// import 'reflect-metadata';
-// import {
-//   BaseEntity,
-//   Column,
-//   CreateDateColumn,
-//   Entity,
-//   PrimaryColumn,
-//   PrimaryGeneratedColumn,
-//   OneToMany,
-//   ManyToMany,
-//   ManyToOne,
-//   JoinColumn,
-//   Generated,
-// } from 'typeorm';
-// import { User } from './User';
-// import { Product } from './Product';
-// import { PurchasedItem } from './PurchasedItem';
-
-// @Entity()
-// export class PurchasedOrder extends BaseEntity {
-//   @PrimaryGeneratedColumn()
-//   order_id!: number;
-
-//   @Column()
-//   user_id!: number;
-
-//   @CreateDateColumn()
-//   createdAt!: Date;
-
-//   @ManyToOne((type) => User, (user) => user.id)
-//   @JoinColumn({ name: 'user_id' })
-//   user!: User;
-
-//   @OneToMany((type) => PurchasedItem, (item) => item.puchase_id)
-//   items!: PurchasedItem[];
-// }
 import {
   CreationOptional,
   DataTypes,
